9e8e209c92433c95d4c952aae35fc694e3675b01
Galadriel16 <129340753+Galadriel16@users.noreply.github.com>
2023-04-24 19:17:18 -0600

    Update Ejercicio1.py

--- Tarea 2/Ejercicio1.py
+++ Tarea 2/Ejercicio1.py
@@ -1,14 +1,3 @@
 # Ejercicio 1: Factorial de un número
 
-def factor(num):
-    if num < 0:
-        return "Error. Este número es negativo"
-    elif num == 0:
-        return 1
-    else:
-      factor_var = 1
-      for i in range(1, num+1):
-          factor_var = factor_var * i
-    return factor_var
-num = int(input("Escriba un número mayor o igual a 0: \n"))
-print(factor(num))
+# No pude realizar esta práctica
