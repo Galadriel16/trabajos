623174c39ebb058d5ad226e7b7e4de6ba7dcfff6
Galadriel16 <129340753+Galadriel16@users.noreply.github.com>
2023-05-01 02:03:37 -0600

    Add files via upload

--- Tarea 3/Formulas.py
+++ Tarea 3/Formulas.py
@@ -10,24 +10,24 @@ def sum():
         print("¡Valores deben ser mayores a cero!") # validación, los números deben ser mayores a cero
     else:
         suma = 0
-        for i in range(1, numero + 1): 
+        for i in range(1, numero + 1): #ciclo for para introducir los números que el usuario indicó al inicio.
             valor = int(input(f"Escriba el número {i}: "))
-            suma = suma + valor
+            suma = suma + valor 
         print(f"La suma de los números que ha escrito es {suma}")
 
-if __name__ == "__main__":
-    sum()
+if __name__ == "__main__": #lo que entendí es que al usar este código me permite respetar el uso de las variables dentro de la función "sum"
+    sum()  #no habría conflicto al momento de importar la función en otro *.py
 
 # Función de la RESTA
 def rest():
     print("RESTA DE VALORES")
     print("----------------")
-    num1 = int
-    num2 = int
+    num1 = int # se asigna un número y se declara como un entero
+    num2 = int # se asigna un segundo número y se declara como un entero
 
-    num1 = int(input("Escriba el primer número: "))
+    num1 = int(input("Escriba el primer número: ")) # usuario escribe un número y luego repite el procedimiento con el segundo número
     num2 = int(input("Escriba el segundo número: "))
-    resta = int(num1 - num2)
+    resta = int(num1 - num2) # la variable resta guardará el resultado de restar num1 - num2
     print('El resultado de la resta es: ')
     print(resta)
 
@@ -39,12 +39,12 @@ if __name__ == "__main__":
 def multi():
     print("MULTIPLICACIÓN DE VALORES")
     print("---------------")
-    numero = int(input("¿Cuántos valores va a introducir? "))
+    numero = int(input("¿Cuántos valores va a introducir? ")) #tiene la misma lógica que la función sum
 
-    if numero <= 0:
-        print("¡Valores deben ser mayores a cero!")
+    if numero <= 0: 
+        print("¡Valores deben ser mayores a cero!") #valida que los valores no sean menores o iguales a cero
     else:
-        multiplica = 1
+        multiplica = 1 # se asigna el valor de 1 y no 0, ya que al multiplicar cero por cualquier valor que introduzca el usuario me daba cero, por eso se le asigna 1
         for i in range(1, numero + 1):
             valor = int(input(f"Escriba el número {i}: "))
             multiplica = multiplica * valor
@@ -57,12 +57,12 @@ if __name__ == "__main__":
 def divi():
     print("DIVISIÓN DE VALORES")
     print("----------------")
-    num1 = int
+    num1 = int #similar al de la función de resta
     num2 = int
 
     num1 = int(input("Escriba el primer número: "))
     num2 = int(input("Escriba el segundo número: "))
-    division = float(num1 / num2)
+    division = float(num1 / num2) # se utilizan en lugar de enteros, números decimales, ya que muchas veces los resultados con división arrojan esos valores.
     print('El resultado de la división es: ')
     print(division)
 
@@ -73,10 +73,10 @@ if __name__ == "__main__":
 def factor():
     print("FACTORIAL DE UN VALOR")
     print("---------------------")
-    numero = int(input("Escriba un número entero mayor que cero: "))
+    numero = int(input("Escriba un número entero mayor que cero: ")) #similar a la función suma y multiplicación.
 
     if numero <= 0:
-        print("¡Error! El número no es un entero")
+        print("¡Error! El número no es un entero") #valida que el número no sea un número igual o menor a cero
     else:
         factorial = 1
         for i in range(1, numero + 1):
@@ -90,15 +90,15 @@ if __name__ == "__main__":
 def poten():
     print("POTENCIA DE UN VALOR")
     print("---------------------")
-    base = int(input("Escriba el número base: "))
+    base = int(input("Escriba el número base: ")) #usuario introduce un número para la base y otro para el exponente
     expo = int(input("Escriba el número del exponente: "))
 
-    if base == 0 and expo == 0:
+    if base == 0 and expo == 0: # si ambos números son cero, entonces aparece un mensaje de error
         return 'No está definida'
-    elif base != 0 and expo == 0:
+    elif base != 0 and expo == 0: # si la base es diferente a cero, pero el exponente es cero, debe arrojar siempre un valor de 1
         return 1
     else:
-        potencia = 1
+        potencia = 1 #cuando el valor de la potencia se le asigna el valor de 1, entonces se crea un ciclo for para que se multiplique la base por la cantidad de veces que corresponde al exponente
         for i in range(expo):
             potencia = potencia * base
         print(f"La potencia es {potencia}.")
