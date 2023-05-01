da8d86f78f2e2a98c4068d43d46858c0414bcc4e
Galadriel16 <129340753+Galadriel16@users.noreply.github.com>
2023-05-01 01:41:52 -0600

    Add files via upload

--- Tarea 3/Formulas.py
+++ Tarea 3/Formulas.py
@@ -1,29 +1,107 @@
-# Módulo que contiene las fórmulas aritméticas y que se llama desde el archivo Principal.py
+#Módulos de fórmulas matemáticas
+
+#Función de la SUMA
+def sum():
+    print("SUMA DE VALORES")
+    print("---------------")
+    numero = int(input("¿Cuántos valores va a introducir? ")) # cantidad de números que se van a sumar
+
+    if numero <= 0:
+        print("¡Valores deben ser mayores a cero!") # validación, los números deben ser mayores a cero
+    else:
+        suma = 0
+        for i in range(1, numero + 1): 
+            valor = int(input(f"Escriba el número {i}: "))
+            suma = suma + valor
+        print(f"La suma de los números que ha escrito es {suma}")
+
+if __name__ == "__main__":
+    sum()
+
+# Función de la RESTA
+def rest():
+    print("RESTA DE VALORES")
+    print("----------------")
+    num1 = int
+    num2 = int
+
+    num1 = int(input("Escriba el primer número: "))
+    num2 = int(input("Escriba el segundo número: "))
+    resta = int(num1 - num2)
+    print('El resultado de la resta es: ')
+    print(resta)
+
+if __name__ == "__main__":
+    rest()
 
 
-def suma(): #se define una función para la fórmula de la suma
-    num = int(input('Escriba un número: '))
-    sum = 0
-while num  >= 0:
-    sum += num
-    num = int(input ("Escriba otro número: "))
-     sum += num
-    if num <
-        return "¿Desea introducir otro número?"
-    elif num == 0:
+# Función de la MULTIPLICACIÓN
+def multi():
+    print("MULTIPLICACIÓN DE VALORES")
+    print("---------------")
+    numero = int(input("¿Cuántos valores va a introducir? "))
+
+    if numero <= 0:
+        print("¡Valores deben ser mayores a cero!")
+    else:
+        multiplica = 1
+        for i in range(1, numero + 1):
+            valor = int(input(f"Escriba el número {i}: "))
+            multiplica = multiplica * valor
+        print(f"La multiplicación de los números que ha escrito es {multiplica}")
+
+if __name__ == "__main__":
+    multi()
+
+# Función de la DIVISIÓN
+def divi():
+    print("DIVISIÓN DE VALORES")
+    print("----------------")
+    num1 = int
+    num2 = int
+
+    num1 = int(input("Escriba el primer número: "))
+    num2 = int(input("Escriba el segundo número: "))
+    division = float(num1 / num2)
+    print('El resultado de la división es: ')
+    print(division)
+
+if __name__ == "__main__":
+    divi()
+
+# Función del FACTORIAL
+def factor():
+    print("FACTORIAL DE UN VALOR")
+    print("---------------------")
+    numero = int(input("Escriba un número entero mayor que cero: "))
+
+    if numero <= 0:
+        print("¡Error! El número no es un entero")
+    else:
+        factorial = 1
+        for i in range(1, numero + 1):
+            factorial = factorial * i
+        print(f"El factorial de {numero} es {factorial}.")
+
+if __name__ == "__main__":
+    factor()
+
+# Función de la POTENCIA
+def poten():
+    print("POTENCIA DE UN VALOR")
+    print("---------------------")
+    base = int(input("Escriba el número base: "))
+    expo = int(input("Escriba el número del exponente: "))
+
+    if base == 0 and expo == 0:
+        return 'No está definida'
+    elif base != 0 and expo == 0:
         return 1
-    else 
+    else:
+        potencia = 1
+        for i in range(expo):
+            potencia = potencia * base
+        print(f"La potencia es {potencia}.")
 
-# def main():
-#     print("SUMA DE NÚMEROS")
-#     numero = int(input("Escriba un número: "))
-#     suma = 0
-#     while numero >= 0:
-#         suma += numero
-#         numero = int(input("Escriba otro número: "))
-#     print()
-#     print(f"La suma de los números positivos introducidos es {suma}.")
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    poten()
--- Tarea 3/Principal.py
+++ Tarea 3/Principal.py
@@ -1,15 +1,63 @@
 # Tarea 3: Calculadora
 
 #Se importa el módulo que contiene las operaciones artiméticas que se realizarán en el programa
+import Formulas
+
+#Formulas.sum()
+#Formulas.resta
 
 # Se crea un menú para que el usuario escoja la operación que desea realizar
 
-print('Selecciona en el siguiente menú la operación que querés realizar: ')
-print('1. Suma')
-print('2. Resta')
-print('3. Multiplicación')
-print('4. División')
-print('5. Factorial')
-print('6. Potencia')
+while 1:
+    print('C A L C U L A D O R A ')
+    print('----------------------')
+    print('Selecciona en el siguiente menú la operación que querés realizar: ')
+    print('1. Suma')
+    print('2. Resta')
+    print('3. Multiplicación')
+    print('4. División')
+    print('5. Factorial')
+    print('6. Potencia')
+    print('7. Salir')
 
-opcion = int(input ('¿Cuál operación aritmética querés realizar? '))
+    n = int(input ('¿Cuál opción vas a escoger? '))
+
+    if n == 1:
+        resultado = Formulas.sum() #se realiza la fórmula de cada módulo proveniente del archivo Formulas.py
+        file = open("resultados.txt","a") #se crea un archivo .txt que muestra la operación escogida por el usuario.
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 1: SUMA ")
+        file.write("------------------------------")
+        file.close()
+    elif n == 2:
+        resultado = Formulas.rest()
+        file = open("resultados.txt","a")
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 2: RESTA ")
+        file.write("-------------------------------")
+        file.close()
+    elif n == 3:
+        resultado = Formulas.multi()
+        file = open("resultados.txt","a")
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 3: MULTIPLICACIÓN ")
+        file.write("------------------------------------------")
+        file.close()
+    elif n == 4:
+        resultado = Formulas.divi()
+        file = open("resultados.txt","a")
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 4: DIVISIÓN ")
+        file.write("------------------------------------")
+        file.close()
+    elif n == 5:
+        resultado = Formulas.factor()
+        file = open("resultados.txt","a")
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 5: FACTORIAL ")
+        file.write("-------------------------------------")
+        file.close()
+    elif n == 6:
+        resultado = Formulas.poten()
+        file = open("resultados.txt","a")
+        file.write(" USUARIO ESCOGIÓ OPCIÓN 6: POTENCIA ")
+        file.write("------------------------------------")
+        file.close()
+    elif n == 7:
+        print('------Terminaste de usar la calculadora-------')
+        break
